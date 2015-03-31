a396b76067871f28852382df3aef193c3ecff808
Aurélien Gâteau <mail@agateau.com>
2015-03-31 17:32:12 +0200

    Log import results in a dir and add a view to check them

--- batch/incwo/urls.py
+++ batch/incwo/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns
 
 incwo_urls = patterns('batch.incwo.views',
                       (r'^/?$', 'imports'),
+                      (r'^/(?P<log_dir>[-:T0-9]+)$', 'details'),
                       )
